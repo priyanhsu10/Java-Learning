ea6238de5333f928f6d161a487b2a04010fe7f81
WebSamurai <priyanshuparate@yahoo.in>
2021-07-25 12:28:17 +0530

    added department

---
+++ src/com/company/practice/Department.java
@@ -0,0 +1,11 @@
+package com.company.practice;
+
+public class Department {
+    private int id;
+    private String name;
+
+    public Department(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
